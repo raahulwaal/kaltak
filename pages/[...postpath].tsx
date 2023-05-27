07874f9c9383722d8a60601a867a6cc9e963500d
raahulwaal <133681855+raahulwaal@users.noreply.github.com>
2023-05-27 07:59:05 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://kaaltak.000webhostapp.com/` + encodeURI(path as string)
+					`http://kaltak.great-site.net/` + encodeURI(path as string)
 				}`,
 			},
 		};
